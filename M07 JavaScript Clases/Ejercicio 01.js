d8a8cc0d753fa150d9e4210c48198eaf5f2f84a4
HarryPlaysXP <133687433+HarryPlaysXP@users.noreply.github.com>
2023-09-28 22:02:42 -0300

    MODULO 7 TERMINADO
    MODULO 7 TERMINADO

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,18 +10,56 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+
+// SE USA EXPRESION POR CLASE PERO SE PDORIA USAR EL METODO VIEJO PONIENDO 
+// function Usuario (usuario , nombre,email , password) {
+//    this.usuario = usuario
+//    this.nombre = nombre
+//    this.email = email
+//    this.password = password
+   // function saludar (){
+   //       return "Hola, mi nombre es " + this.nombre 
+   //       // tambien se puede poenr "Hola, mi nombre es ${this.nombre}"
+   //       // tuve el error que en la parte del strig  no habia puesto el espacio al final por eso daba error
+   //    }
+// }
+   // CODIGO OK la saque casi a la primera
+   class Usuario {
+      constructor (usuario , nombre , email , password) {
+         this.usuario = usuario
+         this.nombre = nombre
+         this.email = email
+         this.password = password
+      }
+      saludar (){
+         return "Hola, mi nombre es " + this.nombre 
+         // tambien se puede poenr "Hola, mi nombre es ${this.nombre}"
+         // tuve el error que en la parte del strig  no habia puesto el espacio al final por eso daba error
+      }
+   }
+   return Usuario
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   // CODIGO OK vi el video porque no encontraba el erro
+
+   Usuario.prototype.saludar = function (){ // me habia faltado poner que el prototype era igual a la function
+         return "Hello World!"
+   }
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   // tuve que ver el video para hacerlo
+   String.prototype.reverse = function () {
+      return this.split("").reverse().join("") // habia puesto (" ") y tenia que ser sin espacio
+// se usan metodos de JavaScript "split", "reverse" y "join" ya que se combierte al String en array y se da vuelta sus caracteres   
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,13 +6,36 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
-}
+
+   // CODIGO OK PRACTICAMOS EXPRESION POR CLASE la mitad la hice sin ver la videoclase
+   constructor (nombre , apellido , edad , domicilio){
+      this.nombre = nombre
+      this.apellido = apellido
+      this. edad = edad
+      this.domicilio = domicilio
+   }
+   detalle() { // para hacer esta parte tuve qeu ver el video ya que no sabia como poner exactamente esto, peor iba bien encaminado
+      return {
+         nombre : this.nombre,
+         apellido : this.apellido,
+         edad : this.edad,
+         domicilio : this.domicilio
+         }
+      }
+   }
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+
+   // codgio OK tuve que repasar la explicacion de las clases
+   var nuevaInstancia = new Persona (nombre, apellido, edad, domicilio) 
+   return nuevaInstancia
+
+   // en la videoclase lo ahcen asi
+   // return new Persona (nombre, apellido, edad, domicilio)
 }
 
 function agregarMetodo() {
@@ -20,6 +43,11 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+
+   // Ejercico OK, vi el vidoe para entender la consigna pero la resolucion fue mia
+   Persona.prototype.datos = function (nombre, edad) {
+      return  this.nombre + ", "+ this.edad + " años"
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
